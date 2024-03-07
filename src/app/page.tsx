19cef27df1174f998436baba3ef27dd2944e7951
Ankur Goyal <ankrgyl@gmail.com>
2024-03-06 23:34:05 -0800

    Add clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 import { ReleaseNotes } from "@/components/release-notes";
 import { useCompletion } from "ai/react";
 
@@ -8,16 +15,24 @@ export default function Home() {
     api: "/api/generate",
   });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ReleaseNotes
-        onSubmit={(startDate, endDate) =>
-          complete("", {
-            body: { startDate, endDate },
-          })
-        }
-        notes={completion}
-      />
-      {error && <div className="text-red">{`${error}`}</div>}
-    </main>
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <SignOutButton />
+          <ReleaseNotes
+            onSubmit={(startDate, endDate) =>
+              complete("", {
+                body: { startDate, endDate },
+              })
+            }
+            notes={completion}
+          />
+          {error && <div className="text-red">{`${error}`}</div>}
+        </main>
+      </SignedIn>
+    </ClerkProvider>
   );
 }
